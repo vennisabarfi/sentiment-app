87ea4d919aa8ec05ed3eb9af594b8fdc9cdc1f8e
veyhn <vennisabarfi@gmail.com>
2024-11-18 22:20:21 -0500

    Set up tailwind and shadcn. Working on frontend designs and api designs tomorrow

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button"
+import "./App.css"
+
+export default function App() {
+  return (
+   <>
+   <div className="center">
+   <Button className="button">Help me. Clickity Click</Button>
+   </div>
+
+   <div>
+    
+   </div>
+  
+
+
+   
+   </>
+  )
+}
