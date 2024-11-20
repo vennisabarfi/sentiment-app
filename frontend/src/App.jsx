85a4fb76e186fdead39f885e614879af6bda2651
veyhn <vennisabarfi@gmail.com>
2024-11-20 18:35:15 -0500

    setting up backend and ui designs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button"
+
 import "./App.css"
 
 export default function App() {
   return (
    <>
    <div className="center">
+
    <Button className="button">Help me. Clickity Click</Button>
    </div>
 
