a074cb8690c7bfded204f3b5435f3f93e8eaff7c
veyhn <vennisabarfi@gmail.com>
2024-11-21 16:03:06 -0500

    successfully integrated huggingface model and added api endpoint. off to integrating with db so need to create endpoints and then add integration tests

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ import psycopg2
 from dotenv import find_dotenv, load_dotenv
 import sys, os
 from flask_migratepg import MigratePg
+from models import sentiment_model, emotion_sentiment_model
+
 
 app = Flask(__name__)
 
@@ -13,12 +15,12 @@ if not load_dotenv(find_dotenv()):
 else:
     print(".env file loaded successfully!") 
 
+#migrating to database
 app.config.from_mapping(
     MIGRATIONS_PATH=os.path.abspath('database/migrations'),
     PSYCOPG_CONNINFO=os.getenv("DATABASE_URL")
 )
 
-print(os.getenv("DATABASE_URL"))
 MigratePg(app)
 
 
@@ -32,6 +34,39 @@ def databaseConnection():
         print("Error establishing database connection", err)
         sys.Exit(1)
     return conn, cur
+
+
+
+# initialize sentiment model
+sentiment_pipeline = sentiment_model()
+
+#initialize emotion-sentiment model
+emotion_sentiment_pipeline = emotion_sentiment_model()
+
+# Use the model to analyze text
+result = sentiment_pipeline("Yeah this product sucks and I want a new one. Please refund my money")
+emotion= emotion_sentiment_pipeline("This sucks ass")
+print(result[0]["label"])
+print(emotion)
+
+
+# get the emotional sentiment of a comment
+@app.route("/sentiment", methods=["POST"])
+def get_sentiment():
+    try:
+        data = request.json
+        if not data or "comment" not in data:
+            return jsonify({"message": "Invalid input, 'comment' field is required"}), 400
+
+        comment = data["comment"]
+        comment_feeling = emotion_sentiment_pipeline(comment)
+        return jsonify({"message":comment_feeling[0]["label"] }), 200
+    except ValueError as err:
+        return jsonify({"message": err}),400
+    except Exception as e:
+         return jsonify({"message": f"An unexpected error occurred: {str(e)}"}), 500
+
+  
 
 
 @app.route("/health", methods=["GET"])
---
+++ backend/models/__init__.py
@@ -0,0 +1,3 @@
+from .models import sentiment_model, emotion_sentiment_model
+
+__all__ = ["sentiment_model", "emotion_sentiment_model"] #controls what gets imported from models
