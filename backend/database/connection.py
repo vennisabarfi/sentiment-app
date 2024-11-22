1442c08be15a1747bb4c1b6ca83e8fd25127ec83
veyhn <vennisabarfi@gmail.com>
2024-11-22 17:08:17 -0500

    expnded api endpoints with better error handling for comments table.Working on testing and frontend next.

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,16 +1,22 @@
 import os
 import sys
 import psycopg2
+from psycopg2.pool import SimpleConnectionPool
 
 
 
 # establish database connection
 def databaseConnection():
     try:
-        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        conn_pool = SimpleConnectionPool(minconn=1, #minimum number of connections
+                                    maxconn=10, #maximum number of connections
+                                    dsn = os.getenv("DATABASE_URL"))
+        # get connection from pool
+        conn = conn_pool.getconn()
+        #initialize cursor
         cur = conn.cursor()
         print("Database connection successful!")
     except psycopg2.OperationalError as err:
         print("Error establishing database connection", err)
-        sys.Exit(1) #exit program if the connection fails
-    return conn, cur
+        sys.exit(1) #exit program if the connection fails
+    return conn_pool, conn, cur
